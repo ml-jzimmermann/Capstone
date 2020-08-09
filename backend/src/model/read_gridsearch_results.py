72529dbf6513186587f10c5e1b028bbd6c32a138
julius <zimmermann.julius@web.de>
2020-08-09 19:25:15 +0200

    add gridsearch results for google;

--- backend/src/model/read_gridsearch_results.py
+++ backend/src/model/read_gridsearch_results.py
@@ -1,7 +1,24 @@
 import pickle
+import matplotlib.pyplot as plt
 
+data = None
+with open('gridsearch_results_google.save', 'rb') as file:
+    data = pickle.load(file)
 
-with open('gridsearch_results.save', 'rb') as file:
-    data_dict = pickle.load(file)
-    print(data_dict)
+class_dict = {True: '--', False: '-'}
+spread_dict = {0.025: 'red', 0.02: 'blue', 0.015: 'yellow', 0.01: 'green', 0.005: 'magenta'}
 
+for run, results in data.items():
+    history = data[run]['history']
+    acc = history['accuracy']
+    val_acc = history['val_accuracy']
+    configuration = data[run]['configuration']
+    four_classes = configuration['four_classes']
+    spread = configuration['spread']
+    plt.plot(acc, class_dict[four_classes], color=spread_dict[spread])
+    # plt.plot(val_acc, class_dict[four_classes], color=spread_dict[spread])
+
+plt.title('Accuracy per Epoch')
+plt.xlabel('Epochs')
+plt.ylabel('Accuracy')
+plt.show()
